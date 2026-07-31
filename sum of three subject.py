b5d57a3628c11484b508036298ff14d619e5dc4d
tharshni145-coder <tharshni145@gmail.com>
2026-07-31 11:14:40 +0530

    code

---
+++ sum of three subject.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the tamil mark:"))
+b=int(input("Enter the english mark:"))
+c=int(input("Enter the maths value:"))
+total_mark=a+b+c
+print("total mark")
+average = total_mark/3
+print("average mark")
---
+++ task.py
@@ -0,0 +1,10 @@
+a=[10,23,34,16,3,2]
+odd=0
+even=0
+for i in a:
+  if i%2==0:
+    even+=1
+  else:
+    odd+=1
+print("odd number is",odd)
+print("even number is",even)
